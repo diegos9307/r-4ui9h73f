c0149d4de6530986b5af309e85704f2433c47089
Diego A. Suaza R <diegos9307@gmail.com>
2022-03-30 22:56:17 -0500

    creando un repetidor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      copy: "",
+    };
+
+    this.repetidor = this.repetidor.bind(this);
+  }
+
+  repetidor(e) {
+    this.setState({
+      copy: e.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input
+          type="text"
+          onChange={this.repetidor}
+          placeholder="Empieza a escribir algo"
+        />
+        <p className="repeater">{this.state.copy}</p>
       </div>
     );
   }
